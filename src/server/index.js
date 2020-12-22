b7b5759ff5863e2202d7894eaf1704058560963b
Nghia Nguyen <naguyen22@gmail.com>
2020-12-22 15:23:33 -0500

    clean up server index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,40 +1,31 @@
-var path = require('path')
-const express = require('express');
+var path = require("path");
+const express = require("express");
 const bodyParser = require(`body-parser`);
-const callAPI = require('./mockAPI.js')
+const callAPI = require("./mockAPI.js");
 
 // Environment variable setup
-const dotenv = require('dotenv').config();
+const dotenv = require("dotenv").config();
 const API_KEY = process.env.API_KEY;
 
-const app = express()
+const app = express();
 
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static('dist'))
-
-// app.get('/', function (req, res) {
-//     // res.sendFile('dist/index.html')
-//     res.sendFile(path.resolve('src/client/views/index.html'))
-// })
+app.use(express.static("dist"));
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+	console.log("Example app listening on port 8080!");
+});
 
-app.post('/test', function (req, res) {
+app.post("/test", function (req, res) {
+	const mood = req.body.mood;
 
-    const mood = req.body.mood;
-
-    // send mood to API
-    callAPI(API_KEY, mood)
-        .then(res => JSON.parse(res))
-        .then(data => res.send(data))
-        .catch(err => console.log(`error on the server: ${err}`))
-
-    // receipt confirmation
-    // res.send(`mood received`);
-})
+	// send mood to API
+	callAPI(API_KEY, mood)
+		.then((res) => JSON.parse(res))
+		.then((data) => res.send(data))
+		.catch((err) => console.log(`error on the server: ${err}`));
+});
